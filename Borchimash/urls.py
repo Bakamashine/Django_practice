d19252783ff373fb51d73c3ba2202946cc6b8bb0
Bakamashine <[200~surajaaminu09@gmail.com>
2025-05-28 09:48:49 +0300

    update .gitignore

--- Borchimash/urls.py
+++ Borchimash/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("main.urls")),
-    path("", include("RegAuth.urls"))
+    path("", include("RegAuth.urls")),
+    path("products/", include("product.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
