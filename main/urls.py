eef11b12a98ed105225659d6db459b3e6b7df291
Bakamashine <[200~surajaaminu09@gmail.com>
2025-05-28 16:03:08 +0300

    Обновил страницу "О нас"

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from main.views import index
+from main.views import index, about_us
 
 urlpatterns = [
-    path("", index, name="main")
+    path("", index, name="main"),
+    path("about_us/", about_us, name="about_us")
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,6 @@ from news.models import News
 def index(req):
     news = News.objects.all().values("date", 'text', 'id').order_by("-date")[:4]
     return render(req, "main/index.html", {"news": news})
+
+def about_us(req):
+    return render(req, 'main/about_us.html')
