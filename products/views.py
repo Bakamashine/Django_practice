3688773bdf0a926a5140150fdc827512d292533c
Bakamashine <surajaaminu09@gmail.com>
2025-05-30 01:15:00 +0300

    Добавил пагинацию в категории

--- products/views.py
+++ products/views.py
@@ -3,15 +3,33 @@ import os
 from Borchimash.settings import BASE_DIR, STATIC_URL
 from django.http import HttpRequest
 from products.models import Product, Category
+from django.core.paginator import Paginator
 
-FILES_ROOT = os.path.join(STATIC_URL, 'models')
+FILES_ROOT = os.path.join(STATIC_URL, "models")
+
 
 def index(req: HttpRequest):
-    return render(req, "products/index.html", {'categories': Category.objects.all()})
+    category = Category.objects.all()
+    paginator = Paginator(category, 6)
+    if "page" in req.GET:
+        page_num = req.GET["page"]
+    else:
+        page_num = 1
+    page = paginator.get_page(page_num)
+    return render(
+        req, "products/index.html", {"page": page, "categories": page.object_list}
+    )
+
 
 def product(req: HttpRequest, category: int):
     products = Product.objects.filter(category=category)
     category_name = Category.objects.get(pk=category)
 
-    return render(req, 'products/products/product.html', {"title": category_name, "products": products,})
-        
+    return render(
+        req,
+        "products/products/product.html",
+        {
+            "title": category_name,
+            "products": products,
+        },
+    )
