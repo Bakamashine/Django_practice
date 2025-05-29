569f34e8aca40eb853d641f9d24a6b34b811fd0d
Bakamashine <[200~surajaaminu09@gmail.com>
2025-05-29 10:23:25 +0300

    Обновил файлы

--- assets/test.js
+++ assets/test.js
@@ -1,16 +1,21 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from "three";
+import { Curves, OrbitControls } from "three/examples/jsm/Addons.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // camera.position.z = 5;
 
-camera.position.set(0,1,5);
+camera.position.set(453, 124, 228);
 
 const controles = new OrbitControls(camera, renderer.domElement);
 controles.enableDamping = true;
@@ -19,20 +24,37 @@ controles.enableZoom = true;
 
 const loader = new GLTFLoader();
 
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // Мягкий свет
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1); // Направленный свет
+directionalLight.position.set(5, 5, 5);
+
 loader.load(
-  '/static/models/girl.glb',
+  "/static/models/Output.glb",
   function (gltf) {
     scene.add(gltf.scene);
+    scene.add(ambientLight);
+    scene.add(directionalLight);
+    
+    gltf.animations; // Array<THREE.AnimationClip>
+    gltf.scene; // THREE.Group
+    gltf.scenes; // Array<THREE.Group>
+    gltf.cameras; // Array<THREE.Camera>
+    gltf.asset; // Object
   },
-  undefined,
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
   function (error) {
-    console.error('An error happened during the model load:', error);
+    console.error("Ошибка загрузки модели:", error);
   }
 );
 
 function animate() {
+  // console.log(camera.position)
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
 }
+
 
 animate();
