853bd604732695176b48c0b91923c7871437335a
Bakamashine <[200~surajaaminu09@gmail.com>
2025-06-02 15:46:34 +0300

    Переделаваю кнопки в шапке

---
+++ main/static/js/header_menu.js
@@ -0,0 +1,25 @@
+const menu_button = document.querySelectorAll("#menu-button");
+menu_button.forEach((elem) => {
+  /**
+   * @type {HTMLButtonElement}
+   */
+  let button = elem.children["click"];
+
+  /**
+   * @type {HTMLElement}
+   */
+  let modal_window = elem.children["modal_window"];
+
+  console.log("button: ", button);
+  console.log("modal_window", modal_window);
+  console.log("elem_children", elem.children);
+
+  let isActive = modal_window.classList.contains("show");
+  console.log(isActive)
+
+  button.addEventListener("click", () => {
+    !isActive
+      ? modal_window.classList.replace("show", "hidden")
+      : modal_window.classList.replace("hidden", "show");
+  });
+});
