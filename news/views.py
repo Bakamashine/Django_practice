70e035410b315c5e7378c72217b9a3f950a92c35
Bakamashine <[200~surajaaminu09@gmail.com>
2025-06-04 16:02:00 +0300

    Удалил комментарий

--- news/views.py
+++ news/views.py
@@ -12,28 +12,3 @@ def detail(req: HttpRequest, one_news: int):
 def year(req: HttpRequest, year: int):
     news = News.objects.filter(date__year__exact=year).order_by('-date')
     return render(req, 'news/year.html', {"news": news, "year": year})
-
-
-# def detail(req: HttpRequest, one_news: int):
-#     news = News.objects.get(pk=one_news)
-#     images: List[Images] = news.images.all()
-#     years = []
-#     dates = News.objects.all().values('date')
-
-#     # TODO: Работает..
-#     for n in dates:
-#         try: 
-#             my_date = datetime.strftime(n['date'], "%Y")
-#             if years.index(my_date):
-#                 years.append(my_date)
-#         except ValueError:
-#             years.append(my_date)
-#         except:
-#             continue
-        
-
-#     return render(req, "news/detail.html", {"one_news": news, "images": images, "years": years})
-
-# def year(req: HttpRequest, year: int):
-#     # news = News.objects.filter(date=)
-#     return render(req, 'news/year.html')
